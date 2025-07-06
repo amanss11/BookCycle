e4c08daafbf8d40b46681a23b804588c0a4496e0
Aman Shaikh <shaikhzams123@gmail.com>
2025-07-06 20:13:00 +0530

    added filter dropdown

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -54,7 +54,7 @@ const Footer = () => {
                 </div>
 
                 <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-300">
-                    <p>&copy; 2024 BookCycle. All rights reserved.</p>
+                    <p>&copy; 2025 BookCycle. All rights reserved.</p>
                 </div>
             </div>
         </footer>
